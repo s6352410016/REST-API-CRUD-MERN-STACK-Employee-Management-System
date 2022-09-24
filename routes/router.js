1eb7a188260f803c9d933481cf8a143e35101f01
s6352410016 <s6352410016@sau.ac.th>
2022-09-24 14:54:38 +0700

    make it better

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
     destination: (req , file , cb) => {
-        cb(null , './routes');
+        cb(null , './client/imageUpload');
     },
     filename: (req , file , cb) => {
         const fileExtension = file.mimetype.split('/')[1];
