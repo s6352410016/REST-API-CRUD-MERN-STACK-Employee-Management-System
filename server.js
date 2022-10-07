f4739e468373430a7064cb282cc3cba1785f0960
s6352410016 <s6352410016@sau.ac.th>
2022-10-07 15:55:03 +0700

    make it better

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const router = require('./routes/router');
 const path = require('path');
 const app = express();
 
-app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use( (req , res , next) => {
+    res.setHeader('Content-Type' , 'application/json');
+    res.setHeader('Access-Control-Allow-Origin' , '*');
+    res.setHeader('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
+    res.setHeader('Access-Control-Allow-Headers' , 'Content-Type');
+    next();
+})
 app.use(router);
 
 if(process.env.NODE_ENV === 'production'){
