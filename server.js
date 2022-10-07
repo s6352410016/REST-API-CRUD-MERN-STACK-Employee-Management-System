670bd987b225b80b3e1c276f0f6c985a85574a7f
s6352410016 <s6352410016@sau.ac.th>
2022-10-07 16:16:48 +0700

    make it better

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use( (req , res , next) => {
     // res.header('Content-Type' , 'application/form-data');
     res.header('Access-Control-Allow-Origin' , '*');
-    res.header('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
-    // res.header('Access-Control-Allow-Headers' , 'Content-Type');
+    // res.header('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
+    res.header('Access-Control-Allow-Headers' , 'Content-Type');
     next();
 })
 app.use(router);
