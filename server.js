4243abeb2d482590b25b3cb4e1ebe09c4face1e7
s6352410016 <s6352410016@sau.ac.th>
2022-10-07 16:02:49 +0700

    make it better

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 app.use( (req , res , next) => {
-    res.setHeader('Content-Type' , 'application/form-data');
-    res.setHeader('Access-Control-Allow-Origin' , '*');
-    res.setHeader('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
-    res.setHeader('Access-Control-Allow-Headers' , 'Content-Type');
+    res.header('Content-Type' , 'application/form-data');
+    res.header('Access-Control-Allow-Origin' , '*');
+    res.header('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
+    res.header('Access-Control-Allow-Headers' , 'Content-Type');
     next();
 })
 app.use(router);
