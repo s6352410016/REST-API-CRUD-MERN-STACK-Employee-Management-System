faac9bc945304f656b7e4ced8399fc3d27aa5024
s6352410016 <s6352410016@sau.ac.th>
2022-10-07 16:04:50 +0700

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use( (req , res , next) => {
     res.header('Access-Control-Allow-Origin' , '*');
     res.header('Access-Control-Allow-Methods' , 'GET , POST , PUT , DELETE');
     res.header('Access-Control-Allow-Headers' , 'Content-Type');
-    next();
+    return next();
 })
 app.use(router);
 
