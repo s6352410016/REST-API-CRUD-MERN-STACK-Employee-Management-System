9e0f785c48f31e40f00e6dff7c45908aad90709d
s6352410016 <s6352410016@sau.ac.th>
2022-10-07 16:35:55 +0700

    make it better

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const router = require('./routes/router');
 const path = require('path');
 const app = express();
 
-app.use( (req , res , next) => {
-    res.header('Access-Control-Allow-Origin' , '*');
-    res.header('Access-Control-Allow-Headers' , 'Content-Type');
-    res.header('Access-Control-Allow-Methods' , '*');
-    next();
-})
+app.use(cors());
 app.use(router);
 
 if(process.env.NODE_ENV === 'production'){
