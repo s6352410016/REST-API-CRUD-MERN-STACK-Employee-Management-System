ccf50318f9ca62997bc41c797dc750e7b56a662e
s6352410016 <s6352410016@sau.ac.th>
2023-01-13 17:24:42 +0700

    last Updated On 13/01/2023 17:24

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(router);
 
-app.use(express.static(path.join(__dirname , './client/build')));
+app.use(express.static(path.join(__dirname , './client/public')));
 app.get('*' , (req , res) => {
-    res.sendFile(path.join(__dirname , './client/build/index.html'));
+    res.sendFile(path.join(__dirname , './client/public/index.html'));
 });
 
 app.listen(process.env.PORT || 5000 , () => {
